b45f92f946c65b688989e01db36fecd01c0e27a0
pho0m <kiattiphoom@utotech.org>
2024-09-14 22:30:37 +0700

    fix: 🐛 added new template

--- routes/web.php
+++ routes/web.php
@@ -1,54 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\BlogController;
 
-// Define a variable for the client path
-$clientPath = '/clients';
+//นักอ่าน
+Route::get('/',[BlogController::class,'index']);
+Route::get('/detail/{id}',[BlogController::class,'detail']);
 
-
-// user view
-Route::get('/', function () use ($clientPath) {
-    return view($clientPath . '/index');
+//นักเขียน
+Route::prefix('author')->group(function(){
+    Route::get('/blog',[AdminController::class,'index'])->name('blog');
+    Route::get('/create',[AdminController::class,'create']);
+    Route::post('/insert',[AdminController::class,'insert']);
+    Route::get('/delete/{id}',[AdminController::class,'delete'])->name('delete');
+    Route::get('/change/{id}',[AdminController::class,'change'])->name('change');
+    Route::get('/edit/{id}',[AdminController::class,'edit'])->name('edit');
+    Route::post('/update/{id}',[AdminController::class,'update'])->name('update');
 });
+Auth::routes();
 
-Route::get('/shop', function () use ($clientPath) {
-    return view($clientPath . '/shop');
-});
-
-Route::get('/product-details', function () use ($clientPath) {
-    return view($clientPath . '/product-details');
-});
-
-Route::get('/cart', function () use ($clientPath) {
-    return view($clientPath . '/cart');
-});
-
-Route::get('/checkout', function () use ($clientPath) {
-    return view($clientPath . '/checkout');
-});
-
-
-
-// Define a variable for the admins path
-$adminPath = '/admins';
-
-// user view
-Route::get('/admins', function () use ($adminPath) {
-    return view($adminPath . '/index');
-});
-
-Route::get('/admins/sample-page', function () use ($adminPath) {
-    return view($adminPath . '/sample-page');
-});
-
-Route::get('/admins/product-details', function () use ($adminPath) {
-    return view($adminPath . '/product-details');
-});
-
-Route::get('/admins/cart', function () use ($adminPath) {
-    return view($adminPath . '/cart');
-});
-
-Route::get('/admins/checkout', function () use ($adminPath) {
-    return view($adminPath . '/checkout');
-});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,5 @@
 let mix = require("laravel-mix");
 
-mix.js("resources/js/app.js", "dist/app.js").postCss(
-    "resources/css/app.css",
-    "dist/css"
-);
+mix.js().postCss("resources/css/app.css", "dist/css");
 
 mix.browserSync("127.0.0.1:8000");
