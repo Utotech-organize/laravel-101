393d19e88bc54b69ad22743ad78ec07a03f3be4b
phuwis <phuwis.tweed@gmail.com>
2024-09-15 00:59:01 +0700

    fix: 🐛 header-bar
    fixed text header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
   <div id="app" class="d-flex flex-column min-vh-100">
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
       <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">Bookshelf</a>
+        <a class="navbar-brand" href="{{ url('/') }}">Bookshelf Application</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
           <span class="navbar-toggler-icon"></span>
